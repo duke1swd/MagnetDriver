750b0bd2c5b36b734505d54282d71e1b2d11c53a
duke1swd <swd@pobox.com>
2020-03-04 12:54:43 -0500

    Added a command line interface

--- rpi/readpin.c
+++ rpi/readpin.c
@@ -5,10 +5,67 @@
 #include <gpiod.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+
+#define	MAXPIN	53
 
 char *myname;
+int pin;
 
-static int pin(unsigned int p) {
+static void
+set_defaults()
+{
+	pin = 0;
+}
+
+static void
+usage()
+{
+	set_defaults();
+	fprintf(stderr, "Usage: %s <options>\n",
+		myname);
+	fprintf(stderr, "Options:\n");
+	fprintf(stderr, "\t-p <pin number (%d)>\n", pin);
+	exit(1);
+}
+
+static void
+grok_args(int argc, char **argv) {
+	int c;
+	int nargs;
+	int errors;
+
+	errors = 0;
+	set_defaults();
+	while ((c = getopt(argc, argv, "p:")) != EOF)
+	switch (c) {
+		case 'p':
+			pin = atoi(optarg);
+			break;
+		default:
+			usage();
+	}
+
+	nargs = argc - optind;
+
+	if (pin < 0 || pin > MAXPIN) {
+		fprintf(stderr, "%s: pin number (%d) must be "
+				"in the range [0-%d]\n",
+			myname, pin, MAXPIN);
+		errors++;
+	}
+
+	if (nargs != 0) {
+		fprintf(stderr, "%s: No positional arguments\n",
+			myname);
+		errors++;
+	}
+
+	if (errors)
+		usage();
+}
+
+static int read_pin(unsigned int p) {
 	int r;
 
 	r = gpiod_ctxless_get_value("0", p, 0, "me");
@@ -27,12 +84,11 @@ int
 main(int argc, char **argv)
 {
 	int v;
-	int p;
 	myname = *argv;
 
-	p = 77;
+	grok_args(argc, argv);
 
-	v = pin(p);
-	printf("pin %d is %d\n", p, v);
+	v = read_pin(pin);
+	printf("pin %d is %d\n", pin, v);
 	return 0;
 }
